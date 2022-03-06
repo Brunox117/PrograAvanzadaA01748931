438709e5589c8fcdce736f3f3059d88085102eea
Bruno <omarjm0910@gmail.com>
2022-03-06 14:15:30 -0600

    1

--- ejercicio2.c
+++ ejercicio2.c
@@ -4,7 +4,7 @@ int main()
 {
     unsigned long long x = 1;
     int i = 0;
-    for (i = 0; i < 64; i++ ){
+    for (i = 0; i < 65; i++ ){
       printf("x << %d = %lld\n",i, x << i);
     } 
     return 0;

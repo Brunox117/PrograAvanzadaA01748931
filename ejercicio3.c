6b6f98ee0a2600a73629dbde8347cdeaed5b56ef
Bruno <omarjm0910@gmail.com>
2022-03-06 14:34:45 -0600

    1

--- ejercicio3.c
+++ ejercicio3.c
@@ -9,7 +9,7 @@ int main()
     int j = 0;
     for (i = 0; i < 3; i++ ){
         for (j = 0; j < 3; j++ ){
-           printf("direccion de arr[%d][%d] = ",i,j) 
+           printf("direccion de arr[%d][%d] = ",i,j); 
         }
     }
 }

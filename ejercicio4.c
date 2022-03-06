19238bad172f86439f9e76d3de00a1467ac93914
Bruno <omarjm0910@gmail.com>
2022-03-06 14:53:59 -0600

    1

--- ejercicio4.c
+++ ejercicio4.c
@@ -7,10 +7,10 @@ int y = 10;
     int x2;
     int y2 = 10;
     int arr[10]
-    printf("%d\n", sizeof(x));
-    printf("%d\n", sizeof(y));
-    printf("%d\n", sizeof(x2));
-    printf("%d\n", sizeof(y2));
-    printf("%d\n", sizeof(arr)));
+    printf("%lu\n", sizeof(x));
+    printf("%lu\n", sizeof(y));
+    printf("%lu\n", sizeof(x2));
+    printf("%lu\n", sizeof(y2));
+    printf("%lu\n", sizeof(arr));
     return 0;
 }

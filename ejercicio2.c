9df0088d3179809b1d35dff31b8d087855ed17f9
Bruno <omarjm0910@gmail.com>
2022-03-06 13:53:52 -0600

    12

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){
-  uint64_t var = 1;
+  unsigned long long int var = 1;
   for (i = 0; i < 30; i++ ){
-      x = (var << i )
-      printf(x)
+      x = (var << i );
+      printf(x);
   } 
 }

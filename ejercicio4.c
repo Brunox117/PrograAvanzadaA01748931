c5f005032fd85ef5de90b798db480e6a3f0b5a7c
Bruno <omarjm0910@gmail.com>
2022-03-06 15:02:28 -0600

    1

--- ejercicio4.c
+++ ejercicio4.c
@@ -7,10 +7,10 @@ int main()
     int x2;
     int y2 = 10;
     int arr[10];
-    printf("global sin inicializar %lu\n", sizeof(x));
-    printf("global inicializada %lu\n", sizeof(y));
-    printf("stack sin inicializar %lu\n", sizeof(x2));
-    printf("stack inicializado %lu\n", sizeof(y2));
+    printf("global sin inicializar %d\n", sizeof(x));
+    printf("global inicializada %d\n", sizeof(y));
+    printf("stack sin inicializar %d\n", sizeof(x2));
+    printf("stack inicializado %d\n", sizeof(y2));
     printf("array %lu\n", sizeof(arr));
     return 0;
 }

753630011bcaab1825583c00c475e70a2e9b6074
Bruno <omarjm0910@gmail.com>
2022-03-06 14:52:41 -0600

    1

---
+++ ejercicio4.c
@@ -0,0 +1,16 @@
+//A01748931
+#include <stdio.h>
+int main()
+int x;
+int y = 10;
+{
+    int x2;
+    int y2 = 10;
+    int arr[10]
+    printf("%d\n", sizeof(x));
+    printf("%d\n", sizeof(y));
+    printf("%d\n", sizeof(x2));
+    printf("%d\n", sizeof(y2));
+    printf("%d\n", sizeof(arr)));
+    return 0;
+}

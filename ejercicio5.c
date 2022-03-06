a2995205ffc11bf14a30528ae25d46556706d0c2
Bruno <omarjm0910@gmail.com>
2022-03-06 15:24:46 -0600

    1

--- ejercicio5.c
+++ ejercicio5.c
@@ -1,6 +1,20 @@
 //A01748931
 #include <stdio.h>
-main(){
-    
+void main() {
+    printf("La serie de Leibniz con 1000 iteraciones: %LF \n",serieLeibniz(1000));
+    printf("La serie de Leibniz con 10000 iteraciones: %LF \n",serieLeibniz(10000));
+    printf("La serie de Leibniz con 1000000 iteraciones: %LF \n",serieLeibniz(1000000));
     return 0;
 }
+long double serieLeibniz(int iteraciones) {
+    int signo = 1;
+    int denominador = 1;
+    long double pi = 0;
+    for (int i = 0; i < iterations; i++) {
+        pi += signo * (4/denominador);
+        signo *= -1;
+        denominador += 2;
+    }
+    return pi;
+}
+

84226b4c66d3d059d061555fc98fa763b463b067
Bruno <omarjm0910@gmail.com>
2022-03-06 15:34:30 -0600

    111

--- ejercicio5.c
+++ ejercicio5.c
@@ -1,20 +1,24 @@
 //A01748931
 #include <stdio.h>
-long double serieLeibniz(int iterations) {
-    int signo = 1;
-    int denominador = 1;
-    long double pi = 0;
-    for (int i = 0; i < iterations; i++) {
-        pi = pi + signo * (4/denominador);
-        signo = signo * -1;
-        denominador += 2;
-    }
-    return pi;
+double calcularPi(double iteraciones) {
+  double numerador = 4;
+  double denominador = 1; // A este lo vamos a ir aumentando de 2 en 2
+  double pi = 0;
+  int x = 0;
+  // Recuerda que es alternar entre negativo y positivo; por eso la bandera.
+  // Comenzamos en positivo
+  int operador = 1;
+  for (x; x < iteraciones; x++) {
+    pi += operador * (numerador / denominador);
+    denominador += 2; // El denominador aumenta de 2 en 2
+    operador *= -1;   // Alternamos operador
+  }
+  return pi;
 }
 void main() {
-    printf("La serie de Leibniz con 1000 iteraciones: %lf \n",serieLeibniz(1000));
-    printf("La serie de Leibniz con 10000 iteraciones: %lf \n",serieLeibniz(10000));
-    printf("La serie de Leibniz con 1000000 iteraciones: %lf \n",serieLeibniz(1000000));
+    printf("La serie de Leibniz con 1000 iteraciones: %f \n",serieLeibniz(1));
+    printf("La serie de Leibniz con 10000 iteraciones: %f \n",serieLeibniz(2));
+    printf("La serie de Leibniz con 1000000 iteraciones: %f \n",serieLeibniz(3));
     return 0;
 }
 

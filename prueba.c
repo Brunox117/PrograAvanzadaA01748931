b77a70e2929b7869f6a23295d1fef72427be2b10
Bruno <omarjm0910@gmail.com>
2022-03-07 11:01:24 -0600

    1

--- prueba.c
+++ prueba.c
@@ -9,6 +9,6 @@ int main()
     while(1){
         x++;
         p = malloc(1024);
-        printf("Iteracion %d el tamaño es: %d",x,sizeof(p));
+        printf("Iteracion %d el tamaño es: %ld",x,sizeof(p));
     }
 }

694edfee342e3a2e2932ec98be316225c756f1b5
Bruno <omarjm0910@gmail.com>
2022-03-07 10:30:57 -0600

    l

--- prueba.c
+++ prueba.c
@@ -1,3 +1,4 @@
+//Archivo de pruebas
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -5,7 +6,6 @@ int main()
 {
     for (int i=0; i<10000000; i++)
     {
-       // Allocating memory without freeing it
        int *ptr = (int *)malloc(sizeof(int));
     }
     return 0;

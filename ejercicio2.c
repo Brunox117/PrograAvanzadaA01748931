50e48326c19a276a1722661248b9511b02255882
Bruno <omarjm0910@gmail.com>
2022-04-18 11:23:13 -0500

    0

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,7 +1,34 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
- void main(){
-  pid_t pid
-  exit(0);
+ 
+int main(int argc, char *argv[])
+{
+    pid_t pid1, pid2,pid3;
+    int status1, status2,status3;
+    if((pid3=fork())==0){
+    if ( (pid1=fork()) == 0 )
+    { /* hijo */
+        printf("Soy el primer hijo (%d, hijo de %d)\n",  getpid(), getppid());
+    }
+    else
+    { /*  padre */
+        if ( (pid2=fork()) == 0 )
+        { /* segundo hijo  */
+            printf("Soy el segundo hijo (%d, hijo de %d)\n",  getpid(), getppid());
+        }
+        else
+        { /* padre */
+/* Esperamos al primer hijo */
+            waitpid(pid1, &status1, 0);
+/* Esperamos al segundo hijo */
+            waitpid(pid2, &status2, 0);
+            printf("Soy el padre (%d, hijo de %d)\n", getpid(), getppid());
+        }
+    }
+    }else{
+      wait(&status3)
+      printf("Soy el abuelo (%d)\n",getpid())
+    }
+    return 0;
 }

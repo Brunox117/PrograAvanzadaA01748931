ae3e0acff3bb021c8dcdf1faf085d5eafa15e63c
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-05-09 16:12:31 -0500

    Update programa6.c

--- tareaSemana7/programa6.c
+++ tareaSemana7/programa6.c
@@ -1,38 +1,35 @@
 // Crea un programa que cambie las mayusculas por minusculas y las minusculas por mayusculas
-// en un archivo. En el programa anterior esta el ejemplo para leer argumentos de la linea de
-// comandos. Usa la-luna.txt
+// en un archivo. Usa la-luna.txt.
+
+// Usa standard i/o.
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <ctype.h>
+#include <stdlib.h>
 
-int main(void) {
-    const char* filename = "la-luna.txt";
-    const char* filename2 = "la-luna2.txt";
+int main(int argc, char *argv[]) {
+	FILE *input;
+	int v;
+	char buff[1];
 
-    FILE* input_file = fopen(filename, "r+");
-    if (!input_file)
-        exit(EXIT_FAILURE);
-    FILE* input_file2 = fopen(filename2, "w+");//Con w+ creamos un archivo
-    if (!input_file)
-        exit(EXIT_FAILURE);
-    do{
-      int letra = fgetc(input_file);//Obtiene un caracter del archivo
-      if(letra != EOF){ //Pregunta si el caracter es el final del archivo (-1)
-        char x = (char)letra;
-        if(x >= 'a' && x <= 'z')
-            x = toupper(x);
-        else if(x >= 'A' && x<= 'Z')
-            x = tolower(x);
-        fputc(x,input_file2);//escribimos el caracter en el archivo laluna2
-      }
-    }while(!feof(input_file));
+	input = fopen(argv[1], "r");
+	if (input == NULL) {
+		perror("file");
+		printf("Error, debes proporcionar un archivo\n");
+		exit(-1);
+	}
 
-    fclose(input_file);
-    fclose(input_file2);
-    exit(EXIT_SUCCESS);
+	do {
+		v = fread(buff, 1, 1, input);
+
+		if (buff[0] >= 65 && buff[0] <= 90) {
+			fprintf(stderr, "%c", buff[0] + (97-65));
+		} else if (buff[0] >= 97 && buff[0] <= 122) {
+			fprintf(stderr, "%c", buff[0] - (97-65));
+		} else {
+			fprintf(stderr, "%c", buff[0]);
+		}
+	} while(v > 0);
+
+	
 }
-
-// Usa file stdio.

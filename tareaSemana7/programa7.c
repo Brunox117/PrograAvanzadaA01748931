3b009659e7b700b1e183d87993df2e05a0cbdfcb
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-05-09 16:12:53 -0500

    Update programa7.c

--- tareaSemana7/programa7.c
+++ tareaSemana7/programa7.c
@@ -1,25 +1,32 @@
 // Escribe un programa que cuente la cantidad de lineas en un archivo. Usa la-luna.txt
-#include<stdio.h>
 
-int main(){
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-char ca;
-int cont;
+int main(int argc, char *argv[]) {
+	FILE *input;
+	int v, c=0;
+	char buff[1];
 
-FILE *puntero;
-puntero = fopen("la-luna.txt","r");
+	input = fopen(argv[1], "r");
+	if (input == NULL) {
+		perror("file");
+		printf("Error, debes proporcionar un archivo\n");
+		exit(-1);
+	}
 
-while(1){
+	for(;;) {
+		v = fread(buff, 1, 1, input);
+		if (v <= 0) break;
 
-      ca = fgetc(puntero);
+		if (buff[0] == '\n') {
+			c++;
+		}
+	} 
 
-      if(ca == '\n'){
-          cont++;
-      }
-      if(ca == EOF){ 
-          printf("%d",cont);
-          break;
-      }
-  }
-    return 0;
+	printf("Hay %d lineas\n", c);
+
+	
 }

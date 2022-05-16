d47743101801226142a590f0965d124f898dc84c
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-05-16 13:03:19 -0500

    Update solucion.c

--- examen2/pregunta2/solucion.c
+++ examen2/pregunta2/solucion.c
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-FILE *f;
+FILE *file;
 
 void handler(int x){
     printf("Soy el padre mi PID es: %d\n",getpid());
@@ -12,17 +12,17 @@ void handler(int x){
 
 int main () {
   int n;
-  f = fopen("proceso.pid","w+");
+  file = fopen("proceso.pid","w+");
   if (!fork()){
-    fprintf(f, "%d", getpid());
-    fflush(f);
+    fprintf(file, "%d", getpid());
+    fflush(file);
     signal(SIGUSR1, handler);
     if(!fork()){
       if(!fork()){
         printf("soy el nieto mi PID es: %d y el de mi padre: %d\n", getpid(), getppid());
         sleep(3);
-        f = fopen("proceso.pid","r");
-        fscanf(f, "%d", &n);
+        file = fopen("proceso.pid","r");
+        fscanf(file, "%d", &n);
         kill((pid_t)n,SIGUSR1);
       }
       wait(0);
@@ -30,3 +30,4 @@ int main () {
     wait(0);
   }
 }
+

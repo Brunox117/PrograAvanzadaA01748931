0e9bdda81a2cababce3b80e7c9d42584ecb1ada2
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-05-30 22:00:46 -0500

    Update programa2.c

--- Pipes/programa2.c
+++ Pipes/programa2.c
@@ -1,1 +1,28 @@
 // Usa mkfifo para enviar mensajes cada 10 segundos, con un timestamp 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+int main()
+{
+    int fd;
+    char * myfifo = "/tmp/myfifo";
+
+    /* create the FIFO (named pipe) */
+    mkfifo(myfifo, 0666);
+    int time = 10;
+    int i = 0;
+    while(i<time) {
+          /* write "Hi" to the FIFO */
+          fd = open(myfifo, O_WRONLY);
+          write(fd, "Hi", sizeof("Hi"));
+          close(fd);
+          sleep(10);
+          i++;
+        }
+    /* remove the FIFO */
+    unlink(myfifo);
+
+    return 0;
+}

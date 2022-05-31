41543258f9198c88a9fc8d924113cfb982fa6fd1
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-05-30 22:01:03 -0500

    Update programa3.c

--- Pipes/programa3.c
+++ Pipes/programa3.c
@@ -1,1 +1,23 @@
 // Usa el FIFO del programa anterior, consume los mensajes.
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define MAX_BUF 1024
+
+int main()
+{
+   int fd;
+   char * myfifo = "/tmp/myfifo";
+   char buf[MAX_BUF];
+   while(1){
+   /* open, read, and display the message from the FIFO */
+   fd = open(myfifo, O_RDONLY);
+   read(fd, buf, MAX_BUF);
+   printf("Received: %s\n", buf);
+ }
+   close(fd);
+
+   return 0;
+}
